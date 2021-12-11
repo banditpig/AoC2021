fa882285002797b4a768993185b140fabf076f72
banditpig <mike_k_houghton@yahoo.co.uk>
2021-12-11 09:29:32 +0000

    Day 10 p1

--- out/production/AoC2021/Day10.hs
+++ out/production/AoC2021/Day10.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Day10 where
 
@@ -23,7 +23,7 @@ type LineSymbols = [Symbol]
 type Input = [LineSymbols]
 
 symbolChars :: [Char]
-symbolChars = ['(', ')', '[', ']', '{', '}', '<', '>']
+symbolChars = ['(', ')', '[', ']', '{', '}', '<', '>'] 
 
 symbol :: Parser Symbol
 symbol = do
@@ -33,7 +33,7 @@ symbol = do
 line :: Parser LineSymbols
 line = many symbol <* newline
 
-allLines :: Parser [LineSymbols]
+allLines ::  Parser [LineSymbols]
 allLines = many line
 
 matchingSymbols :: Symbol -> Symbol -> Bool
@@ -44,6 +44,7 @@ matchingSymbols l r
   | (l, r) == (Oarrow, Carrow) = True
   | otherwise = False
 
+
 charToSym :: Char -> Symbol
 charToSym c = maybe Oparen toEnum (elemIndex c symbolChars)
 
@@ -51,11 +52,11 @@ isOpenSymbol :: Symbol -> Bool
 isOpenSymbol s = s `elem` [Oparen, Osquare, Obrace, Oarrow]
 
 cost :: Num p => Symbol -> p
-cost = \case
-  Cparen -> 3
-  Csquare -> 57
-  Cbrace -> 1197
-  Carrow -> 25137
+cost = \case 
+          Cparen  -> 3
+          Csquare -> 57
+          Cbrace  -> 1197
+          Carrow  -> 25137 
 
 processLine :: [Symbol] -> Maybe Symbol
 processLine ls = go ls empty Nothing
@@ -70,13 +71,15 @@ processLine ls = go ls empty Nothing
         goagain = case myb of
           Nothing -> Nothing
           Just s ->
-            if matchingSymbols s sym then go syms stack' err else Just sym
+            if matchingSymbols s sym
+              then go syms stack' err
+              else Just sym
 
-part1 :: Num a => [[Symbol]] -> a
 part1 inp = total
   where
     errs = mapMaybe processLine inp
     total = foldr (\sym acc -> acc + cost sym) 0 errs
+     
 
 part2 = const ()
 
---
+++ out/production/AoC2021/Stack.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TypeApplications #-}
+module Stack where
+import Prelude
+
+data StackState = EmptyStack | NotEmptyStack
+newtype Stack a = Stack [a] deriving (Show, Foldable)
+
+empty :: Stack a
+empty = Stack []
+isEmpty  :: Stack a -> StackState
+isEmpty s = if  null s then  EmptyStack else NotEmptyStack
+
+push :: a -> Stack a -> Stack a
+push x (Stack xs)= Stack (x:xs)
+
+pop :: Stack a -> (Maybe a, Stack a)
+pop (Stack []) = (Nothing, Stack [])
+pop (Stack (x:xs)) = (Just x, Stack xs)
